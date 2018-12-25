e25d85e6c040831d4b5cfe9945c4fe9a3db5394a
miroozyx <miroozyx@163.com>
2018-12-25 16:11:49 +0800

    delete useless annotations

--- checkpoint.py
+++ checkpoint.py
@@ -17,19 +17,10 @@ class StepPreTrainModelCheckpoint(Callback):
 
         # Arguments
             filepath: string, path to save the model file.
-            monitor: quantity to monitor.
             verbose: verbosity mode, 0 or 1.
             save_best_only: if `save_best_only=True`,
                 the latest best model according to
                 the quantity monitored will not be overwritten.
-            mode: one of {auto, min, max}.
-                If `save_best_only=True`, the decision
-                to overwrite the current save file is made
-                based on either the maximization or the
-                minimization of the monitored quantity. For `val_acc`,
-                this should be `max`, for `val_loss` this should
-                be `min`, etc. In `auto` mode, the direction is
-                automatically inferred from the name of the monitored quantity.
             save_weights_only: if True, then only the model's weights will be
                 saved (`model.save_weights(filepath)`), else the full model
                 is saved (`model.save(filepath)`).
