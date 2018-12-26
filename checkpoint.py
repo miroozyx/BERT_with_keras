c61860245c5fb3ba339ac7417fffa1a3e15eed7c
miroozyx <miroozyx@163.com>
2018-12-26 18:05:12 +0800

    add f1 checkpoint on steps

--- checkpoint.py
+++ checkpoint.py
@@ -2,6 +2,7 @@
 import warnings
 import numpy as np
 from keras.callbacks import Callback
+from sklearn.metrics import f1_score, classification_report
 
 
 class StepPreTrainModelCheckpoint(Callback):
@@ -108,6 +109,153 @@ class StepPreTrainModelCheckpoint(Callback):
                 else:
                     if self.verbose > 0:
                         print('\nStep %07d: saving model to %s' % (self.cur_step, filepath))
+                    if self.save_weights_only:
+                        self.model.save_weights(filepath, overwrite=True)
+                    else:
+                        self.model.save(filepath, overwrite=True)
+
+
+class StepF1ModelCheckpoint(Callback):
+    """Save the model after every epoch and scale the learning rate.
+
+    `filepath` can contain named formatting options,
+    which will be filled the value of `step` and
+    keys in `logs` (passed in `on_batch_end`).
+
+    For example: if `filepath` is `weights.{step:07d}-{val_f1:.3f}-{threshold:.3f}.hdf5`,
+    then the model checkpoints will be saved with the epoch number and
+    the validation loss in the filename.
+
+    # Arguments
+        filepath: string, path to save the model file.
+        start_step: the started step of model to eval f1 score.
+        verbose: verbosity mode, 0 or 1.
+        save_best_only: if `save_best_only=True`,
+            the latest best model according to
+            the quantity monitored will not be overwritten.
+        save_weights_only: if True, then only the model's weights will be
+            saved (`model.save_weights(filepath)`), else the full model
+            is saved (`model.save(filepath)`).
+        period: Interval (number of steps) between checkpoints.
+        binary: classification mode, binary or not.
+        threshold_binary: threshold value to recognize 0 or 1 class.
+        pred_batch_size: int. Control the batch size of prediction.
+        model.  keras model. model is applied to predict.
+    """
+
+    def __init__(self, filepath,
+                 start_step=10000,verbose=1,
+                 save_best_only=True, save_weights_only=True, xlen=1,
+                 period=1000, binary=True,
+                 threshold_binary=np.arange(0.005,1.0,0.005),
+                 threshold_verbose=0,
+                 pred_batch_size=512,
+                 model=None
+                 ):
+        super(StepF1ModelCheckpoint, self).__init__()
+        self.verbose = verbose
+        self.filepath = filepath
+        self.save_best_only = save_best_only
+        self.save_weights_only = save_weights_only
+        self.period = period
+        self.steps_since_last_save = 0
+        self.xlen = xlen
+
+        self.binary = binary
+        self.threshold_binary = threshold_binary
+        self.threshold_verbose = threshold_verbose
+        self.pred_batch_size = pred_batch_size
+        self.single_gpu_model = model
+
+        self.monitor_op = np.greater
+        self.best = -np.Inf
+        self.start_step = start_step
+        self.cur_step = 0
+
+        if not isinstance(xlen, (int, np.int)):
+            raise ValueError("The arg: 'xlen' should be integer.")
+        if xlen < 1:
+            raise ValueError("The arg: 'xlen' should be >= 1.")
+
+    def set_model(self, model):
+        if self.single_gpu_model is not None:
+            self.model = self.single_gpu_model
+        else:
+            self.model = model
+
+    def on_batch_begin(self, batch, logs=None):
+        self.cur_step += 1
+
+    def on_batch_end(self, batch, logs=None):
+        if self.cur_step > self.start_step:
+            self.steps_since_last_save += 1
+            if self.steps_since_last_save >= self.period:
+                self.steps_since_last_save = 0
+
+                if self.xlen == 1:
+                    x = self.validation_data[0]
+                    y = self.validation_data[1]
+                else:
+                    x = self.validation_data[0: self.xlen]
+                    y = self.validation_data[self.xlen]
+
+                if self.binary:
+                    val_prob = np.asarray(self.model.predict(x, batch_size=self.pred_batch_size))
+                    if np.shape(val_prob)[1] == 1:
+                        val_prob = val_prob[:, 0]
+                        val_true = y
+                    else:
+                        val_prob = val_prob[:, 1]
+                        val_true = np.argmax(y, axis=1)
+
+                    val_f1s = []
+                    for threshold in self.threshold_binary:
+                        val_pred = np.asarray(val_prob > threshold, dtype=np.int)
+                        val_f1 = f1_score(val_true, val_pred)
+                        val_f1s.append(val_f1)
+                        if self.threshold_verbose > 0:
+                            print("\n-------  threshold_value:{:.2f}  -------".format(threshold))
+                            print(classification_report(val_true, val_pred))
+                    val_f1s = np.array(val_f1s)
+                    val_f1 = val_f1s.max()
+                    threshold_value = self.threshold_binary[val_f1s.argmax()]
+
+                else:
+                    val_pred = np.argmax(np.asarray(self.model.predict(x)), axis=1)
+                    val_true = np.argmax(y, axis=1)
+                    val_f1 = f1_score(val_true, val_pred, average='macro')
+                    threshold_value = 0.
+
+                filepath = self.filepath.format(step=self.cur_step, val_f1=val_f1, threshold=threshold_value)
+                if self.save_best_only:
+                    current = val_f1
+                    if current is None:
+                        warnings.warn('Can save best model only with val_f1 available, '
+                                      'skipping.', RuntimeWarning)
+                    else:
+                        if self.monitor_op(current, self.best):
+                            if self.verbose > 0:
+                                if not self.binary:
+                                    print('\nStep %07d: val_f1 improved from %0.5f to %0.5f,'
+                                          ' saving model to %s'
+                                          % (self.cur_step, self.best, current, filepath))
+                                else:
+                                    print('\nStep %07d: val_f1 improved from %0.5f to %0.5f at threshold_value = %0.3f,'
+                                          ' saving model to %s'
+                                          % (self.cur_step, self.best, current, threshold_value, filepath))
+                            self.best = current
+                            if self.save_weights_only:
+                                self.model.save_weights(filepath, overwrite=True)
+                            else:
+                                self.model.save(filepath, overwrite=True)
+                        else:
+                            if self.verbose > 0:
+                                print('\nStep %07d: val_f1 = %0.5f did not improve from %0.5f' %
+                                      (self.cur_step, current ,self.best))
+                else:
+                    if self.verbose > 0:
+                        print('\nStep %07d: saving model to %s, threshold_value = %0.2f'
+                              % (self.cur_step, filepath, threshold_value))
                     if self.save_weights_only:
                         self.model.save_weights(filepath, overwrite=True)
                     else:
